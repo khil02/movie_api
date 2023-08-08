52bec28b0dcf9d1880d08d9b1866ba737fa6ca09
khil02 <120340087+khil02@users.noreply.github.com>
2023-08-08 19:13:45 -0400

    small corrections

--- index.js
+++ index.js
@@ -314,9 +314,9 @@ app.use((err, req, res, next) => {
     res.status(500).send("Something broke!");
   });
 
-  const port = process.env.PORT || 8080
+  const port = process.env.PORT || 8080;
   app.listen(port, '0.0.0.0', () => {
-    console.log('Your app is listening on port 8080.' + port);
+    console.log('Your app is listening on port ' + port);
   });
 
   
